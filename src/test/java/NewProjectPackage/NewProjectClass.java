a2aad92fa1fec5fd59c93a4644fb6e04995ee4d4
smruti901 <smrutimohapatra567@gmail.com>
2022-07-25 12:53:16 +0530

    new commit

--- src/test/java/NewProjectPackage/NewProjectClass.java
+++ src/test/java/NewProjectPackage/NewProjectClass.java
@@ -4,7 +4,7 @@ public class NewProjectClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hi hello hi hello hi");
+		System.out.println("Hi hello hi hello hi hello");
 
 	}
 
